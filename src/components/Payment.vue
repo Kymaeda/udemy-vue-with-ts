568446336e12b827664bc70b6ebaa550c00cd362
Kymaeda <kyo.m.720@gmail.com>
2022-12-31 09:50:46 +0900

    2022/12/31(v-bind)

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+// FIXME: cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.
+const item1 = 'カレーライス'
+const price1 = 400
+const url1 = 'https://pkg.navitime.co.jp/matsuyafoods/spot/detail?code=0000001506'
+
+const item2 = 'ハンバーグ'
+const price2 = 600
+const url2 = 'https://www.nakaita.com/shop/shop-0-97/'
 </script>
 
 <template>
   <div class="container">
     <h1>最近の支出</h1>
     <div class="payment-item">
-      <label>カレーライス</label>
-      <label>400円</label>
+      <label>{{ item1 }}</label>
+      <label>{{ price1 }}</label>
+      <a :href="url1" target="_blank" rel="noopener noreferrer">bought here</a>
+    </div>
+    <div class="payment-item">
+      <label>{{ item2 }}</label>
+      <label>{{ price2 }}</label>
+      <a :href="url2" target="_blank" rel="noopener noreferrer">bought here</a>
     </div>
   </div>
 </template>
@@ -19,6 +33,7 @@
 }
 
 .payment-item {
+  margin: 8px;
   padding: 20px;
   display: flex;
   justify-content: space-between;
