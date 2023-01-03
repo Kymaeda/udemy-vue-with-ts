ff7d9a12f2ae0125d4f3100e997a554e5d6a4b7e
Kymaeda <kyo.m.720@gmail.com>
2023-01-04 08:06:00 +0900

    v-for(静的なリスト)

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+const tweets = [
+  { id: 1, content: 'ツイートの中身1' },
+  { id: 2, content: 'ツイートの中身2' },
+  { id: 3, content: 'ツイートの中身3' },
+]
 </script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <h1>Tweets</h1>
+    <div class="form-container">
+      <input />
+      <button class="submit">post</button>
+    </div>
+
+    <div class="tweet-container">
+      <ul class="tweet-list">
+        <li v-for="tweet in tweets" :key="tweet.id">
+          <span>{{ tweet.content }}</span>
+          <button>delete</button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .container {
+  width: 1280px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -15,11 +36,23 @@
 .form-container {
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   background-color: aliceblue;
   padding: 24px 0;
   width: 60%;
+  height: 100px;
   margin-bottom: 12px;
   border-radius: 4px;
+}
+
+input {
+  width: 50%;
+  height: 25px;
+  border-radius: 5px;
+}
+
+.tweet-list {
+  list-style: none;
 }
 </style>
